f58e1ad83d2fee3c1b5fef7079ef53ae4304c9d0
Ian <mus5002@aol.com>
2018-01-29 12:15:00 -0500

    restructured

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
-const express = require('express')
-const app = express()
-
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/RESTful')
-
-app.use(bodyParser.json())
-app.use(cors())
-
-app.use('/', require('./routes'))
+const app = require('./app')
 
 const port = process.env.PORT || 3030
 app.listen(port, () => {
