e198ae201ea88a170ccc0303d7dea0f221e26f3d
Ian <mus5002@aol.com>
2018-02-09 09:03:04 -0500

    added cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const db = require('./db')
 
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use('/', require('./routes'))
 
